12307edd1b8a87e574db9271ba0ebd466c8c8fb2
Túlio Rezende <tulio.mamute@hotmail.com>
2017-08-15 13:05:34 -0300

    Criação de Receiver de Media Link e alteração de tipo de programa utilizado

---
+++ AprendendoBlip/MediaLinkMessageReceiver.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using Lime.Protocol;
+using Takenet.MessagingHub.Client.Listener;
+using Takenet.MessagingHub.Client.Sender;
+using Lime.Messaging.Contents;
+using Takenet.MessagingHub.Client;
+
+namespace AprendendoBlip
+{
+    public class MediaLinkMessageReceiver : IMessageReceiver
+    {
+        private readonly IMessagingHubSender _sender;
+
+        public MediaLinkMessageReceiver(IMessagingHubSender sender)
+        {
+            _sender = sender;
+        }
+
+        public async Task ReceiveAsync(Message message, CancellationToken cancellationToken = default(CancellationToken))
+        {
+            MediaLink media = RetornaObjetoMediaLink();
+            await _sender.SendMessageAsync(media, message.From, cancellationToken);
+
+        }
+
+        public MediaLink RetornaObjetoMediaLink()
+        {
+            return new MediaLink() { Uri = new Uri(@"http://wallpaperswide.com/street_fighter_v_2015-wallpapers.html"), Title = "Imagem Street Fighter 5", Type = MediaType.ApplicationJson };
+        }
+    }
+}
