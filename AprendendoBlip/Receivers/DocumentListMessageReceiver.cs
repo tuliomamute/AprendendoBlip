26ba81ad91e312651bc10bc1536202e272ffebef
Túlio Rezende <tulio.mamute@hotmail.com>
2017-08-16 17:10:23 -0300

    Criação de Menu de Links e Envio de Localização

---
+++ AprendendoBlip/Receivers/DocumentListMessageReceiver.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using Lime.Protocol;
+using Takenet.MessagingHub.Client.Listener;
+using Takenet.MessagingHub.Client.Sender;
+using Lime.Messaging.Contents;
+using Takenet.MessagingHub.Client;
+
+namespace AprendendoBlip.Receivers
+{
+    public class DocumentListMessageReceiver : IMessageReceiver
+    {
+        private readonly IMessagingHubSender _sender;
+        public DocumentListMessageReceiver(IMessagingHubSender sender)
+        {
+            _sender = sender;
+        }
+
+        public async Task ReceiveAsync(Message message, CancellationToken cancellationToken = default(CancellationToken))
+        {
+            DocumentList list = RetornaObjetoDocumentList();
+           await _sender.SendMessageAsync(list, message.From, cancellationToken);
+        }
+
+        private DocumentList RetornaObjetoDocumentList()
+        {
+            return new DocumentList()
+            {
+                Header = new DocumentContainer()
+                {
+                    Value = new WebLink()
+                    {
+                        Uri = new Uri("http://br.leagueoflegends.com/"),
+                        Text = "Site League of Legends",
+                        Title = "Site League of Legends",
+                        PreviewUri = new Uri("http://ddragon.leagueoflegends.com/cdn/7.16.1/img/champion/Kayn.png"),
+                        PreviewType = $"{MediaType.DiscreteTypes.Image}/{MediaType.SubTypes.JPeg}"
+                    }
+                },
+                Items = new DocumentContainer[] {
+                   new DocumentContainer(){
+                       Value = new WebLink(){
+                           Title ="Localiza",
+                           Uri =new Uri("https://www.localiza.com/"),
+                           Text ="Bot Oficial da Localiza"
+                       }
+                   },
+                   new DocumentContainer(){
+                       Value = new WebLink(){
+                           Title ="Santander",
+                           Uri =new Uri("https://santander.com.br"),
+                           Text ="Bot Oficial do Santander Brasil"
+                       }
+                   },
+                }
+            };
+        }
+    }
+}
---
+++ AprendendoBlip/Receivers/LocationMessageReceiver.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using Lime.Protocol;
+using Takenet.MessagingHub.Client.Listener;
+using Takenet.MessagingHub.Client.Sender;
+using Lime.Messaging.Contents;
+using Takenet.MessagingHub.Client;
+
+namespace AprendendoBlip.Receivers
+{
+    public class LocationMessageReceiver : IMessageReceiver
+    {
+        private readonly IMessagingHubSender _sender;
+
+        public LocationMessageReceiver(IMessagingHubSender sender)
+        {
+            _sender = sender;
+        }
+
+        public async Task ReceiveAsync(Message message, CancellationToken cancellationToken = default(CancellationToken))
+        {
+            Location location = RetornaObjetoLocation();
+            await _sender.SendMessageAsync(location, message.From, cancellationToken);
+        }
+
+        private Location RetornaObjetoLocation()
+        {
+            return new Location()
+            {
+                Latitude = -19.918899,
+                Longitude = -43.959275,
+                Altitude = 853,
+                Text = "Sede da Take.net"
+            };
+        }
+    }
+}
