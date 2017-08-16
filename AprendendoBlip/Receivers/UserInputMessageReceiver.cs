dd14cb311a1030b0b4d06e30665f2d84bb9bb98f
Túlio Rezende <tulio.mamute@hotmail.com>
2017-08-16 17:38:25 -0300

    Criação de Pedido de INput de usuário

---
+++ AprendendoBlip/Receivers/UserInputMessageReceiver.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using Lime.Protocol;
+using Takenet.MessagingHub.Client.Listener;
+using Takenet.MessagingHub.Client.Sender;
+using Lime.Messaging.Contents;
+using Takenet.MessagingHub.Client;
+
+namespace AprendendoBlip.Receivers
+{
+    public class UserInputMessageReceiver : IMessageReceiver
+    {
+        private readonly IMessagingHubSender _sender;
+        public UserInputMessageReceiver(IMessagingHubSender sender)
+        {
+            _sender = sender;
+        }
+
+        public async Task ReceiveAsync(Message message, CancellationToken cancellationToken = default(CancellationToken))
+        {
+            Input input = RetornaObjetoInput();
+            await _sender.SendMessageAsync(input, message.From, cancellationToken);
+        }
+
+        private Input RetornaObjetoInput()
+        {
+            return new Input()
+            {
+                Label = new DocumentContainer()
+                {
+                    Value = "Qual sua localização?"
+                },
+                Validation = new InputValidation()
+                {
+                    Rule = InputValidationRule.Type,
+                    Type = Location.MediaType
+                }
+            };
+        }
+    }
+}
